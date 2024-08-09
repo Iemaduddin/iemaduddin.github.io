5c46b3bbe9ec4eeb1e01d7aad94100bade798ed4
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2024-08-09 08:21:27 +0700

    modify mobile display

--- js/custom.js
+++ js/custom.js
@@ -189,26 +189,26 @@ $(window).on("load", function () {
   /* ======= Mobile Filter ======= */
 
   // bind filter on select change
-  //   $(".portfolio-filter-mobile").on("change", function () {
-  //     // get filter value from option value
-  //     var filterValue = this.value;
-  //     // use filterFn if matches value
-  //     filterValue = filterFns[filterValue] || filterValue;
-  //     $container.isotope({ filter: filterValue });
-  //   });
+  $(".portfolio-filter-mobile").on("change", function () {
+    // get filter value from option value
+    var filterValue = this.value;
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
 
-  //   var filterFns = {
-  //     // show if number is greater than 50
-  //     numberGreaterThan50: function () {
-  //       var number = $(this).find(".number").text();
-  //       return parseInt(number, 10) > 50;
-  //     },
-  //     // show if name ends with -ium
-  //     ium: function () {
-  //       var name = $(this).find(".name").text();
-  //       return name.match(/ium$/);
-  //     },
-  //   };
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function () {
+      var number = $(this).find(".number").text();
+      return parseInt(number, 10) > 50;
+    },
+    // show if name ends with -ium
+    ium: function () {
+      var name = $(this).find(".name").text();
+      return name.match(/ium$/);
+    },
+  };
 });
 
 $(document).on("ready", function () {
